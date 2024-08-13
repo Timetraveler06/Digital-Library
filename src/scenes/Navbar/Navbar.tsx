1572bb42a1d81a14877241c6850768c947c2dab1
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-14 01:18:28 +0300

    Motion div created

--- src/scenes/Navbar/Navbar.tsx
+++ src/scenes/Navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import { PricingContent } from './Components/PricingContent';
 import { ResourceContent } from './Components/ResourceContent';
 import { SolutionsContent } from './Components/SolutionsContent';
 import { FlyoutLink } from './FlyOutLink';
+import { motion } from 'framer-motion';
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -30,74 +31,106 @@ const Navbar: React.FC = () => {
   }, []);
 
   return (
-    <div className={`transition-all ease-in ${isFixed ? 'fixed top-0 z-50 w-full  shadow-md' : 'relative'}`}>
-      {/* Desktop Navbar */}
-      <div className={`hidden md:flex h-[100px] md:bg-black  justify-between items-start px-3 py-12 ${isFixed ? 'bg-white shadow-md' : 'bg-transparent'}`}>
-        <div className="flex px-5 whitespace-nowrap font-extrabold text-white">
-          <a href="/">BRESC Library</a>
-        </div>
-        <div className="flex px-36 gap-24 ">
-          <FlyoutLink FlyoutContent={SolutionsContent}>
-            Solutions
-          </FlyoutLink>
-          <FlyoutLink  FlyoutContent={ResourceContent}>
-            Resources
-          </FlyoutLink>
-          <FlyoutLink  FlyoutContent={AboutContent}>
-            About Us
-          </FlyoutLink>
-          <FlyoutLink  FlyoutContent={ContactContent}>
-            Contact Us
-          </FlyoutLink>
-          <FlyoutLink  FlyoutContent={PricingContent}>
-            Pricing
-          </FlyoutLink>
-        </div>
-      </div>
-
-      {/* Mobile Navbar */}
-      <div className="flex md:hidden items-center justify-between p-4 bg-neutral-900">
-        <button onClick={toggleMenu} className="text-white">
-          {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
-        </button>
-
-        <div
-          className={`fixed top-0 right-0 h-full w-36 bg-neutral-900 transition-transform ${
-            isOpen ? 'translate-x-0' : 'translate-x-full'
-          }`}
-        >
-          <div className="p-4">
-            <ul className="flex flex-col gap-9 py-11">
-              <li>
-                <FlyoutLink  FlyoutContent={SolutionsContent}>
-                  Solutions
-                </FlyoutLink>
-              </li>
-              <li>
-                <FlyoutLink  FlyoutContent={ResourceContent}>
-                  Resources
-                </FlyoutLink>
-              </li>
-              <li>
-                <FlyoutLink  FlyoutContent={AboutContent}>
-                  About Us
-                </FlyoutLink>
-              </li>
-              <li>
-                <FlyoutLink FlyoutContent={ContactContent}>
-                  Contact Us
-                </FlyoutLink>
-              </li>
-              <li>
-                <FlyoutLink FlyoutContent={PricingContent}>
-                  Pricing
-                </FlyoutLink>
-              </li>
-            </ul>
+    <nav className={`transition-all ease-in ${isFixed ? 'fixed top-0 z-50 w-full  shadow-md' : 'relative'}`}>
+      
+                {/* Desktop Navbar */}
+     
+        <motion.div
+          className="mx-auto w-full"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}>
+     
+            
+          <div className={`hidden md:flex h-[100px] md:bg-black  justify-between items-start px-3 py-12 ${isFixed ? 'bg-white shadow-md' : 'bg-transparent'}`}>
+            <div className="flex px-5 whitespace-nowrap font-extrabold text-white">
+              <a href="/">BRESC Library</a>
+            </div>
+            <div className="flex px-36 gap-24 ">
+              <FlyoutLink FlyoutContent={SolutionsContent}>
+                Solutions
+              </FlyoutLink>
+              <FlyoutLink  FlyoutContent={ResourceContent}>
+                Resources
+              </FlyoutLink>
+              <FlyoutLink  FlyoutContent={AboutContent}>
+                About Us
+              </FlyoutLink>
+              <FlyoutLink  FlyoutContent={ContactContent}>
+                Contact Us
+              </FlyoutLink>
+              <FlyoutLink  FlyoutContent={PricingContent}>
+                Pricing
+              </FlyoutLink>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
+        </motion.div>
+
+
+                  {/* Mobile Navbar */}
+        <motion.div
+          className="mx-auto w-full"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+          }}
+        
+        >
+      
+          <div className="flex md:hidden items-center  p-4 bg-neutral-900">
+            <button onClick={toggleMenu} className="text-white">
+              {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+            </button>
+            <div className="flex px-7 whitespace-nowrap font-extrabold text-white">
+              <a href="/">BRESC Library</a>
+            </div>
+            <div
+              className={`fixed top-0 right-0 h-full w-36 bg-neutral-900 transition-transform ${
+                isOpen ? 'translate-x-0' : 'translate-x-full'
+              }`}
+            >
+              <div className="p-4">
+                <ul className="flex flex-col gap-9 py-11">
+                  <li>
+                    <FlyoutLink  FlyoutContent={SolutionsContent}>
+                      Solutions
+                    </FlyoutLink>
+                  </li>
+                  <li>
+                    <FlyoutLink  FlyoutContent={ResourceContent}>
+                      Resources
+                    </FlyoutLink>
+                  </li>
+                  <li>
+                    <FlyoutLink  FlyoutContent={AboutContent}>
+                      About Us
+                    </FlyoutLink>
+                  </li>
+                  <li>
+                    <FlyoutLink FlyoutContent={ContactContent}>
+                      Contact Us
+                    </FlyoutLink>
+                  </li>
+                  <li>
+                    <FlyoutLink FlyoutContent={PricingContent}>
+                      Pricing
+                    </FlyoutLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+    </nav>
   );
 };
 
--- src/scenes/pages/ResourcePage/Resource.tsx
+++ src/scenes/pages/ResourcePage/Resource.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Resource = () => {
   return (
-    <section className='bg-slate-600 w-full h-[700px] text-white'>Resource</dsec>
+    <section className='bg-slate-600 w-full h-[700px] text-white'>Resource</section>
   )
 }
 
