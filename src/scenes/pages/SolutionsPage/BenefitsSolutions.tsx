87b48d47b870804797c11f073e9c9cf4100cbe17
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-20 22:11:46 +0300

    updaing

--- src/scenes/pages/SolutionsPage/BenefitsSolutions.tsx
+++ src/scenes/pages/SolutionsPage/BenefitsSolutions.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const BenefitsSolutions = () => {
   return (
-    <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>BenefitsSolutions</section>
+    <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
+      <div></div>
+    </section>
   )
 }
 
