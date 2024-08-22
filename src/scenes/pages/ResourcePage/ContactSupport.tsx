349b663eef57de58cee9aa9a956aef595223c6e0
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-22 20:33:24 +0300

    updating

--- src/scenes/pages/ResourcePage/ContactSupport.tsx
+++ src/scenes/pages/ResourcePage/ContactSupport.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ContactSupport = () => {
   return (
-    <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>ContactSupport</section>
+    <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
+      <div></div>
+    </section>
   )
 }
 
