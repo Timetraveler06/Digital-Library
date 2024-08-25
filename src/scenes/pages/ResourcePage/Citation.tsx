24ca75381be320b5201b3013b43c8bfd191b2151
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-25 12:08:20 +0300

    Update Citation.tsx

--- src/scenes/pages/ResourcePage/Citation.tsx
+++ src/scenes/pages/ResourcePage/Citation.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Citation = () => {
   return (
     <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
-      
+      <div>
+        
+      </div>
     </section>
   )
 }
