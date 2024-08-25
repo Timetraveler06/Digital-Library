196acaeccbb66a33507f9742884e50ee6eed988a
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-25 12:07:17 +0300

    Update Intro.tsx

--- src/scenes/pages/PricingPage/Intro.tsx
+++ src/scenes/pages/PricingPage/Intro.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Intro = () => {
   return (
     <section className='bg-slate-600 w-full h-[700px] v text-white'>
-      <div></div>
+      <div>
+        <h1>About Us</h1>
+        <p>This is the Introduction Page Content.</p>
+      </div>
     </section>
   )
 }
