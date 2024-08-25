fccbb8135cfb4d506f48af91c5dc787f8a341029
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-25 12:04:06 +0300

    Update About.tsx

--- src/scenes/pages/AboutPage/About.tsx
+++ src/scenes/pages/AboutPage/About.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 const About: React.FC = () => {
   return (
     <section className='dark:bg-gray-900 py-[100px] w-full h-[700px] text-white'>
-      
+      <div>
+        <h1>About Us</h1>
+        <p>This is the About page content.</p>
+      </div>
     </section>
   );
 };
