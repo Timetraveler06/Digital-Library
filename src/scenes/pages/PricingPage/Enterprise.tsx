1bc7561966258fb67aac5fe5b5ce0d52a2dc938d
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-25 12:05:18 +0300

    Update Enterprise.tsx

--- src/scenes/pages/PricingPage/Enterprise.tsx
+++ src/scenes/pages/PricingPage/Enterprise.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Enterprise = () => {
   return (
     <section className='bg-slate-600 w-full h-[700px] py-[100px] text-white'>
-      <div></div>
+      <div>
+        <h1>Enterprises</h1>
+        <p>This is the Enterprises Page .</p>
+      </div>
     </section>
   )
 }
