67b272cd6accbc906138606dda3136add2bb675c
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-25 12:08:26 +0300

    Update Citation.tsx

--- src/scenes/pages/ResourcePage/Citation.tsx
+++ src/scenes/pages/ResourcePage/Citation.tsx
@@ -4,7 +4,8 @@ const Citation = () => {
   return (
     <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
       <div>
-        
+        <h1>About Us</h1>
+        <p>This is the About page content.</p>
       </div>
     </section>
   )
