b917d2c0a7cd98de651c70fcaf01703334c93d7b
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-25 12:04:17 +0300

    Update Collab.tsx

--- src/scenes/pages/ContactPage/Collab.tsx
+++ src/scenes/pages/ContactPage/Collab.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Collab = () => {
   return (
-    <section className=' dark:bg-gray-900 w-full h-[700px] py-[100px] text-white'>Collab</section>
+    <section className=' dark:bg-gray-900 w-full h-[700px] py-[100px] text-white'>
+      <div></div>
+    </section>
   )
 }
 
