42dc9afac4b740eb17af35bc81aa07b1ea6ccf37
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-26 09:23:38 +0300

    Update Guides.tsx

--- src/scenes/pages/ResourcePage/Guides.tsx
+++ src/scenes/pages/ResourcePage/Guides.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Guides = () => {
   return (
     <section className='dark:bg-gray-900 py-[100px] w-full h-[700px] text-white'>
-      
+      <div>
+        <h1>Guides Section</h1>
+      </div>
     </section>
   )
 }
