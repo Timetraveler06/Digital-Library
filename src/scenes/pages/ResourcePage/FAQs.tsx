fc3c4fb8838c1e9dad356407b4e03f428b07a108
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-26 09:24:36 +0300

    Update FAQs.tsx

--- src/scenes/pages/ResourcePage/FAQs.tsx
+++ src/scenes/pages/ResourcePage/FAQs.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const FAQs = () => {
   return (
     <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
-      
+      <div>
+        <h1>FAQs Section</h1>
+      </div>
     </section>
   )
 }
