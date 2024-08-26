476d4c16c9997c990f90e44a5f258fc1852adcf5
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-26 09:25:20 +0300

    Update Search.tsx

--- src/scenes/pages/ResourcePage/Search.tsx
+++ src/scenes/pages/ResourcePage/Search.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Search = () => {
   return (
     <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
-      
+      <div>
+        <h1>Search Section</h1>
+      </div>
     </section>
   )
 }
