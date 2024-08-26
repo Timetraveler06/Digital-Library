e9e7fc3b79d8b89e442b188d9f479bf078b7ac9e
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-26 09:24:47 +0300

    Update Tutorial.tsx

--- src/scenes/pages/ResourcePage/Tutorial.tsx
+++ src/scenes/pages/ResourcePage/Tutorial.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Tutorial = () => {
   return (
-    <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>Tutorial</section>
+    <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
+      
+    </section>
   )
 }
 
