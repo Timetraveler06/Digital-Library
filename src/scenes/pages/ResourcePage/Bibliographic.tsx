69ab53f1cf44ff99852d2cdbd14a04065732c8e9
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-26 09:24:10 +0300

    Update Bibliographic.tsx

--- src/scenes/pages/ResourcePage/Bibliographic.tsx
+++ src/scenes/pages/ResourcePage/Bibliographic.tsx
@@ -5,7 +5,7 @@ const Bibliographic = () => {
     <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
       <div>
     
-        <h1>Catalog Section</h1>
+        <h1>Bibliographic  Section</h1>
       
       </div>
     </section>
