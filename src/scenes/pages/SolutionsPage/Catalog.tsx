c75fca04c133bdad9daad25e56263dacda850ad7
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-26 09:23:00 +0300

    Update Catalog.tsx

--- src/scenes/pages/SolutionsPage/Catalog.tsx
+++ src/scenes/pages/SolutionsPage/Catalog.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Catalog = () => {
   return (
     <section className='dark:bg-gray-900 py-[100px]w-full h-[700px]  text-white'>
-      <div></div>
+      <div>
+        <h1>Catalog Section</h1>
+      </div>
     </section>
   )
 }
