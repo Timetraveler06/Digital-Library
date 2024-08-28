96d4d0f8283ec5395ff0e243349b8fcfb8eb28b4
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-28 17:51:33 +0300

    ada

--- src/scenes/pages/ContactPage/Phone.tsx
+++ src/scenes/pages/ContactPage/Phone.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const phone = () => {
   return (
-    <section className='bg-slate-600 w-full h-[700px] text-white'>phone</section>
+    <section className=' dark:bg-gray-900 py-[100px] w-full h-[700px] text-white'>phone</section>
   )
 }
 
