660415f93a7a4f20bd405044edf68125b1a85a90
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-28 17:54:10 +0300

    Update Practices.tsx

--- src/scenes/pages/ResourcePage/Practices.tsx
+++ src/scenes/pages/ResourcePage/Practices.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Practices = () => {
   return (
     <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
-      
+      <div></div>
     </section>
   )
 }
