4aef7d06ce0bdd25cf63bb1209b51d27201d4865
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-28 17:52:52 +0300

    Update StartUp.tsx

--- src/scenes/pages/PricingPage/StartUp.tsx
+++ src/scenes/pages/PricingPage/StartUp.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const StartUp = () => {
   return (
     <section className='bg-slate-600 w-full h-[700px] py-[100px] text-white'>
-      <div></div>
+      <div>
+        <p>Start Ups</p>
+      </div>
     </section>
   )
 }
