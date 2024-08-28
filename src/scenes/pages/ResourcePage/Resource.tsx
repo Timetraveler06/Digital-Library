70b3e1c36489480ad13effbe631baff6e43358ae
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-28 17:51:40 +0300

    Update Resource.tsx

--- src/scenes/pages/ResourcePage/Resource.tsx
+++ src/scenes/pages/ResourcePage/Resource.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Resource = () => {
   return (
-    <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'></section>
+    <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
+      <div></div>
+    </section>
   )
 }
 
