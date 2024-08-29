acfbbbf3da46b504fc06675b50bee6e316040890
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-30 01:15:18 +0300

    Update TailoredSystems.tsx

--- src/scenes/pages/SolutionsPage/TailoredSystems.tsx
+++ src/scenes/pages/SolutionsPage/TailoredSystems.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const TailoredSystems = () => {
   return (
-    <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>TailoredSystems</section>
+    <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
+      
+    </section>
   )
 }
 
