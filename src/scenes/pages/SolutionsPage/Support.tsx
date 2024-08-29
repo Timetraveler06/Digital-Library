563bd374c81dde12fea3990f2797f106e98542ac
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-08-30 01:15:08 +0300

    Update Support.tsx

--- src/scenes/pages/SolutionsPage/Support.tsx
+++ src/scenes/pages/SolutionsPage/Support.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Support = () => {
   return (
     <section className=' dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
-      <div></div>
+      <div>
+        <p>Support </p>
+      </div>
     </section>
   )
 }
