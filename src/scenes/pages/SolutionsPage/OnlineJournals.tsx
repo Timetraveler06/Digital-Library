ff729b19823e7a88249e5addedaf9f53e4cde12c
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-09-10 19:55:44 +0300

    Update OnlineJournals.tsx

--- src/scenes/pages/SolutionsPage/OnlineJournals.tsx
+++ src/scenes/pages/SolutionsPage/OnlineJournals.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Onlineournals = () => {
   return (
-    <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>Onlineournals</section>
+    <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>
+      
+    </section>
   )
 }
 
