08236ac598578f9eba07d04e8a34048612df033a
Josi <101510934+Timetraveler06@users.noreply.github.com>
2024-09-29 05:04:58 +0300

    Update Ebooks.tsx

--- src/scenes/pages/SolutionsPage/Ebooks.tsx
+++ src/scenes/pages/SolutionsPage/Ebooks.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 const Ebooks = () => {
   return (
+   <div>
+        <section className='dark:bg-gray-900 py-[100px] w-full h-[700px]  text-white'>Ebooks</section>
+
+   </div>
   )
 }
 
